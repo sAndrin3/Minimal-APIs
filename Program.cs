7b16afaf5ffd10641e4d986d8918637fdfc46485
sAndrin3 <sandrinemalanga2001@gmail.com>
2025-09-22 16:16:07 +0300

    Redis cache

--- Program.cs
+++ Program.cs
@@ -7,7 +7,12 @@ using MinimalApi.Entities;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddOutputCache();
+// builder.Services.AddOutputCache();
+builder.Services.AddStackExchangeRedisOutputCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer("name=defaultConnection"));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
